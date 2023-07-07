59e0a04bb3c51ef1578aba60d224019fb2f92bfe
=SarahMumbua <mumbuasarah2030@gmail.com>
2023-07-07 21:19:07 +0300

    mofied About.js component

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../css/About.css'; 
+import '../css/About.css';
 
 const About = () => {
-  return (
-    <div className="about-container">
-      <h1 className="about-heading">Task Manager App</h1>
-      <p className="about-description">
-        It allows users to create tasks, view their tasks, update tasks, delete tasks, update tasks, and mark tasks as Complete or Incomplete.
-      </p>
-      <p className="about-login-link">
-        <Link to="/login" className="login-link">Login</Link> to get started.
-      </p>
-    </div>
-  );
+    return (
+        <div className="about-container">
+            <h1 className="about-heading">Task Manager App</h1>
+            <p className="about-description">
+                It allows users to create tasks, view their tasks, update tasks, delete tasks, update tasks, and mark tasks as Complete or Incomplete.
+            </p>
+            <p className="about-login-link">
+                <Link to="/login" className="login-link">Login</Link> to get started.
+            </p>
+        </div>
+    );
 };
 
 export default About;
