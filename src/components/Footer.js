bc1d548fe21f401dfb5012179df8cf68a4c6b549
=SarahMumbua <mumbuasarah2030@gmail.com>
2023-07-07 21:20:05 +0300

    mofied footer.js component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,11 @@ import React from 'react';
 import '../css/Footer.css';
 
 const Footer = () => {
-  return (
-    <footer className="footer">
-      <p>&copy; 2023 Your Company. All rights reserved.</p>
-    </footer>
-  );
+    return (
+        <footer className="footer">
+            <p>&copy; 2023 Group 7 Task Manager. All rights reserved.</p>
+        </footer>
+    );
 };
 
 export default Footer;
